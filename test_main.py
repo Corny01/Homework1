9282041753b10b7b58b1b153e313483a05b33ce8
11909383 <163847994+11909383@users.noreply.github.com>
2024-04-08 22:33:23 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,16 +1,10 @@
 from main import calculate_rectangle_area
-from main import calculate_cuboid
-from main import main
-
 
 
 def test_calculate_rectangle_area():
-    assert shape_coice(rectangle)
     assert calculate_rectangle_area(1,2) == 2
 
-def test_calculate_cuboid():
-    assert shape_coice(cuboid)
-    assert calculate_cuboid(1,2,3) == 6
+
 
 
 
